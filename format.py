f508861935b7b0cd130900aec3e74837722a2074
seok <exrocc@gmail.com>
2021-01-11 20:28:40 +0900

    Create format.py
    test

---
+++ format.py
@@ -0,0 +1,56 @@
+"""string_a = "{}".format(10)
+
+print(string_a)
+print(type(string_a))"""
+
+"""format_a = "{}만 원" .format(5000)
+format_b = "파이썬 열공해서 {}억 벌기".format(1000)
+
+print(format_a)
+print(format_b)"""
+
+"""output_a = "{:d}".format(52)
+output_b = "{:5d}".format(52)
+output_c = "{:10d}".format(52)
+
+output_d = "{:015d}".format(52)
+output_e = "{:05d}".format(-52)
+
+print("# 기본")
+print(output_a)
+print(output_b)
+print(output_c)
+print("# 0 채우기")
+print(output_d)
+print(output_e)"""
+
+"""output_a = "{:+5d}".format(52)  #+는 따라옴
+output_b = "{:+5d}".format(-52)
+output_c = "{:=+5d}".format(52)  #+는 맨 앞으로
+output_d = "{:=+5d}".format(-52)
+output_e = "{:+05d}".format(52)
+output_f = "{:+05d}".format(-52)
+
+
+print(output_a)
+print(output_b)
+print(output_c)
+print(output_d)
+print(output_e)
+print(output_f)"""
+
+"""output_a="{:15.3f}".format(52.273)
+output_b="{:15.2f}".format(52.273)
+output_c="{:15.1f}".format(52.273)
+
+print(output_a)
+print(output_b)
+print(output_c)"""
+
+"""
+output_a=52.0000
+output_b="{:g}".format(output_a)
+
+print(output_a)
+print(output_b)  #0인 소수점만없에서 보여줌"""
+
